02b2772bf978749c415acb99a8c82d31f4cdfdc1
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2024-03-25 16:53:26 +0300

    django admin

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def  home_screen_view(request):
-    print(request.headers)
-    return render(request, 'base.html')
+    context = {}
+
+    list_of_values = ['First Entry', 'Second Entry', 'Third entry', 'Fourth Entry']
+    context['list_of_values'] = list_of_values
+    
+    return render(request, 'personal/home.html', context)
