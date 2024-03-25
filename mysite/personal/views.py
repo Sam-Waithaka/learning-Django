d365fbde8566948361d05e0da28a035e86810c7d
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2024-03-25 17:17:19 +0300

    render the questions in admin to the Personal view

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def  home_screen_view(request):
     context = {}
 
-    list_of_values = ['First Entry', 'Second Entry', 'Third entry', 'Fourth Entry']
-    context['list_of_values'] = list_of_values
+    # list_of_values = ['First Entry', 'Second Entry', 'Third entry', 'Fourth Entry']
+    # context['list_of_values'] = list_of_values
+
+    questions = Question.objects.all()
+    context['questions'] = questions
+
     
     return render(request, 'personal/home.html', context)
